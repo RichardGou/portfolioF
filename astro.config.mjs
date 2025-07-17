6935620048222dc873740e405bc386a0042519f9
Richad <81878873+RichardGou@users.noreply.github.com>
2025-07-17 14:15:39 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://richardgoudelin.github.io',
   //site: 'https://goudelinrichard.one/',
-  //base: '/portfolioF',
+  base: '/portfolioF',
 })
